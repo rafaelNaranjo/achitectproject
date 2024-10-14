22ca4370aa488da303c28e3d53fa2f2b55993b2e
Rafael Naranjo <rrnp.naranjo@gmail.com>
2024-10-14 23:04:31 +0200

    initial commit architecproject

---
+++ msone/src/main/java/com/rafaelnarnajo/msone/MsoneApplication.java
@@ -0,0 +1,15 @@
+package com.rafaelnarnajo.msone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsoneApplication.class, args);
+    }
+
+}
---
+++ msone/src/test/java/com/rafaelnarnajo/msone/MsoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelnarnajo.msone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
